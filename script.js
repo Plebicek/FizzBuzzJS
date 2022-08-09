2df21d28a4384ae80150430eff08e0104fd55bf9
Vojta <lagt@seznam.cz>
2022-08-09 11:09:59 +0200

    restylied

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* My first project in JavaScript. The project is the game called FizzBuzz 
+where you have to find when a number is Fizz that is when number can be divided by three or Buzz when a number can be divided by Five.
+Third option is when a number can be divided by both that is called FizzBuzz.
+
+Output is in the console.
+
+Maybe later I will update this game. 
+*/
 const input = parseInt(prompt("zadej číslo na hru FizzBuzz"));
 
 let dividedByThree = num=> {
@@ -5,11 +13,13 @@ let dividedByThree = num=> {
         return true;
     }
 }
+
 let dividedByFive = num=> {
     if(num%5==0) {
         return true;
     }
 }
+
 let dividedByThreeAndFive = (num) =>{
     if(num%3==0 && num%5==0) {
         return true;
@@ -17,19 +27,19 @@ let dividedByThreeAndFive = (num) =>{
 }
 
 for (let i = 1; i <= input; i++) {
+
     if(dividedByThreeAndFive(i)) {
         console.log(i + " FizzBuzz")
         continue
     }
+
     if(dividedByFive(i)){
         console.log(i + " Buzz")
         continue
     }
+
     if(dividedByThree(i)){
         console.log(i + " Fizz")
         continue
     }
-    
-    
-
 }
